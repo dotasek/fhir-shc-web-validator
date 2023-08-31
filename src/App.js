65a2672d46493d25294e21b7b9530ff95ad8e98b
dotasek <dotasek.dev@gmail.com>
2023-08-31 10:42:06 -0400

    Add QR Widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {QrScanner} from '@yudiel/react-qr-scanner';
 
 function App() {
   return (
@@ -18,6 +19,13 @@ function App() {
           Learn React
         </a>
       </header>
+        <div className="QR-widget">
+            <QrScanner
+                onDecode={(result) => console.log(result)}
+                onError={(error) => console.log(error?.message)}
+            />
+        </div>
+
     </div>
   );
 }
