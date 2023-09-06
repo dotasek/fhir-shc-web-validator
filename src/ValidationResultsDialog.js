8cb7509bb2bc6a3c14235a2218f082b0250007ed
dotasek <dotasek.dev@gmail.com>
2023-09-06 17:43:07 -0400

    Make the results pretty

--- src/ValidationResultsDialog.js
+++ src/ValidationResultsDialog.js
@@ -2,6 +2,11 @@ import PropTypes from 'prop-types';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import Button from "@mui/material/Button";
+import {List, ListItem, ListItemIcon, ListItemText} from "@mui/material";
+import InfoIcon from '@mui/icons-material/Info';
+import ReportIcon from '@mui/icons-material/Report';
+import ErrorIcon from '@mui/icons-material/Error';
+import WarningIcon from '@mui/icons-material/Warning';
 export default function ValidationResultsDialog(props) {
     const { onClose, validationResults, open } = props;
 
@@ -9,11 +14,36 @@ export default function ValidationResultsDialog(props) {
         onClose(null);
     }
 
+    function getIconForIssueLevel(type) {
+        switch (type) {
+            case "FATAL" : return <ReportIcon></ReportIcon>
+            case "ERROR" : return <ErrorIcon></ErrorIcon>
+            case "WARNING" : return <WarningIcon></WarningIcon>
+            case "INFORMATION" : return <InfoIcon></InfoIcon>
+            default: return undefined;
+        }
+
+    }
 
     return (
         <Dialog onClose={handleClose} open={open}>
             <DialogTitle>Validation Results</DialogTitle>
-            { JSON.stringify(validationResults) }
+
+            {
+                (validationResults && validationResults.outcomes ) &&
+                <List> {
+                validationResults.outcomes[0].issues.map( (issue) => {
+                        console.log("Hey hey hey:" + issue)
+                        return <ListItem>
+                            <ListItemIcon>
+                                { getIconForIssueLevel(issue.level)}
+                            </ListItemIcon>
+                            <ListItemText>{issue.message}</ListItemText>
+                        </ListItem>;
+                    })
+                } </List>
+            }
+
             <Button variant="contained" onClick={handleClose}>Close</Button>
         </Dialog>
     )
